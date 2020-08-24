7f7c7aa355828430b0cf3c0bf8f90ed4e1addb1f
Luca Iacob <luca.iacob.d@gmail.com>
2020-08-24 13:25:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = 'Hello, everybody!';
 
 var specialGuest = 'Niel deGrasse Tyon';
 
-var greetSpecialGuest = "Hello" + specialGuest + "!";
+var greetSpecialGuest = "Hello," + " " + specialGuest + "!";
 
 var topic = "space";
 
